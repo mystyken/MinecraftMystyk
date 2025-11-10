ecc144a9298c8fc8b98daad8d9ebf8dd30af1f97
MattEcodee <mystykgame@outlook.com>
2025-11-10 03:32:08 -0500

    Initial commit

---
+++ src/main/java/com/mystyk/minecraftmystyk/MinecraftMystyk.java
@@ -0,0 +1,16 @@
+package com.mystyk.minecraftmystyk;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinecraftMystyk implements ModInitializer {
+	public static final String MOD_ID = "minecraftmystyk";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/mystyk/minecraftmystyk/MinecraftMystykClient.java
@@ -0,0 +1,10 @@
+package com.mystyk.minecraftmystyk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftMystykClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
