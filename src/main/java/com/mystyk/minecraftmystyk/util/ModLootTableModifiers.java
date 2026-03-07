a4eebd5521e163b1fd3e54155826879e8dd65641
MattEcodee <mystykgame@outlook.com>
2026-03-07 02:57:59 -0500

    1.0.0.2

--- src/main/java/com/mystyk/minecraftmystyk/util/ModLootTableModifiers.java
+++ src/main/java/com/mystyk/minecraftmystyk/util/ModLootTableModifiers.java
@@ -17,10 +17,10 @@ public class ModLootTableModifiers {
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register(((registryKey, builder, lootTableSource, wrapperLookup) -> {
-            if(WARDEN_ID.equals(registryKey.getValue())) {
+            if(LootTables.ANCIENT_CITY_CHEST.equals(registryKey)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(.5f))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f))
                         .with(ItemEntry.builder(ModItems.ECHO_UPGRADE_SMITHING_TEMPLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
@@ -31,7 +31,7 @@ public class ModLootTableModifiers {
             if(WARDEN_ID.equals(registryKey.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(.1f))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f))
                         .with(ItemEntry.builder(ModItems.PULSE_SMITHING_TEMPLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
@@ -42,7 +42,7 @@ public class ModLootTableModifiers {
             if(SHULKER_ID.equals(registryKey.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(.1f))
+                        .conditionally(RandomChanceLootCondition.builder(0.1f))
                         .with(ItemEntry.builder(ModItems.FENDER_SMITHING_TEMPLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
@@ -50,11 +50,11 @@ public class ModLootTableModifiers {
             }
 
 
-            if(LootTables.END_CITY_TREASURE_CHEST.equals(registryKey.getValue())) {
+            if(LootTables.END_CITY_TREASURE_CHEST.equals(registryKey)) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(.02f))
-                        .with(ItemEntry.builder(ModItems.PULSE_SMITHING_TEMPLATE))
+                        .conditionally(RandomChanceLootCondition.builder(0.05f))
+                        .with(ItemEntry.builder(ModItems.ENDER_UPGRADE_SMITHING_TEMPLATE))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
 
                 builder.pool(poolBuilder.build());
